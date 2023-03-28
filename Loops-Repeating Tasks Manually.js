ed371f4606ed074a413f745743afbc4cbe8df3d1
DylanIz <116808456+DylanIz@users.noreply.github.com>
2023-03-28 13:50:57 +0100

    Loops

---
+++ Loops-Repeating Tasks Manually.js
@@ -0,0 +1,25 @@
+/*
+Repeating Tasks Manually
+Before we write our own loops let’s take a moment to develop an appreciation for loops. 
+The best way to do that is by showing you how cumbersome it would be if a repeated task required you to type out the same code every single time.
+*/
+
+//Create the variable vacationSpots, and assign its value to an array of three strings naming places you’d like to visit.
+// Write your code below
+let vacationSpots = [
+    'Spain',
+    'Ireland',
+    'Portugal'];
+    
+//Next, console.log() each item in vacationSpots. Since we don’t know loops yet, we have to console.log() each element in the array separately.
+
+console.log(vacationSpots[0]);
+
+console.log(vacationSpots[1]);
+
+console.log(vacationSpots[2]);
+
+//Nice work! Now imagine that the vacation list had 100 places on it— logging each array element to the console by hand would be a tedious task! 
+//In the next exercise, we will learn how to make things more efficient with for loops.
+
+
